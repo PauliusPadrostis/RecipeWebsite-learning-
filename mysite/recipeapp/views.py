3ac5742b626d6eacbc21186db0011663efb76c5e
Paulius Padrostis <paulius.padrostis94@gmail.com>
2024-01-15 15:17:40 +0200

    Added 4 new recipes into database, so i'd have what to show on the frontend. Deleted old css template and all related files (too complicated).

--- mysite/recipeapp/views.py
+++ mysite/recipeapp/views.py
@@ -5,6 +5,7 @@ from django.views.generic import TemplateView
 
 
 def homepage(request):
-
     return render(request, 'homepage.html', context={})
 
+def about(request):
+    return render(request, 'about.html', context={})
